f6219d100a3be829a0d049b856ffcd54e6ef3e5e
lugoff <florencio.lugo@gmail.com>
2018-08-20 21:38:51 -0500

    final commit

--- week2.playground/Contents.swift
+++ week2.playground/Contents.swift
@@ -2,4 +2,16 @@
 
 import UIKit
 
-var str = "Hello, playground"
+for i in 0...100 {
+    if (i % 5) == 0 {
+        print("# \(i) #Bingo!!!")
+    }
+    if (i % 2) == 0 {
+        print("# \(i) #par")
+    } else {
+        print("# \(i) #impar")
+    }
+    if i >= 30 && i <= 40 {
+        print("# \(i) #Viva Swift!!!")
+    }
+}
