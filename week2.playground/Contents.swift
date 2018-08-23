285b920124431af558897f55f62c0ee30f5b9cf9
lugoff <florencio.lugo@gmail.com>
2018-08-23 13:23:52 -0500

    week 4

--- week2.playground/Contents.swift
+++ week2.playground/Contents.swift
@@ -1,5 +1,5 @@
-//: Playground - noun: a place where people can play
 
+//: Playground - noun: a place where people can play
 import UIKit
 
 for i in 0...100 {
---
+++ week4.playground/Contents.swift
@@ -0,0 +1,50 @@
+//: Semana 4
+
+import UIKit
+
+enum Velocidades : Int {
+    
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init(velocidadInicial : Velocidades) {
+        self = velocidadInicial
+    }
+}
+
+class Auto {
+    var velocidad : Velocidades
+    
+    init() {
+        self.velocidad = .Apagado
+    }
+    
+    func cambioDeVelocidad() -> (actual: Int, velocidadEnCadena: String) {
+        
+        var velocidadEnCadena : String
+        
+        switch self.velocidad {
+        case .Apagado:
+            self.velocidad = .VelocidadBaja
+            velocidadEnCadena = "Velocidad baja"
+        case .VelocidadBaja:
+            self.velocidad = .VelocidadMedia
+            velocidadEnCadena = "Velocidad media"
+        case . VelocidadMedia:
+            self.velocidad = .VelocidadAlta
+            velocidadEnCadena = "Velocidad alta"
+        case .VelocidadAlta:
+            self.velocidad = .VelocidadMedia
+            velocidadEnCadena = "Velocidad media"
+        }
+        return (self.velocidad.rawValue, velocidadEnCadena)
+    }
+}
+
+var auto = Auto()
+print ("\(auto.velocidad.rawValue), \(auto.velocidad)")
+
+for _ in 1...20{                                //prueba de la clase Auto, iterando 20 veces (de 1 a 20)
+    let (actual, velocidadEnCadena) = auto.cambioDeVelocidad()
+    print ("\(actual), \(velocidadEnCadena)")
+}
+
